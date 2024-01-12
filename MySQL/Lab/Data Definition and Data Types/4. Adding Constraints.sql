0c9238392b7caf723d8bb4364e1c4d348decb722
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-01-12 09:47:24 +0200

    Initial Commit

--- MySQL/Lab/Data Definition and Data Types/4. Adding Constraints.sql
+++ MySQL/Lab/Data Definition and Data Types/4. Adding Constraints.sql
@@ -1,9 +1,3 @@
-ALTER TABLE `gamebar`.`products` 
-ADD INDEX `fk_category_id_idx` (`category_id` ASC) VISIBLE;
-;
-ALTER TABLE `gamebar`.`products` 
-ADD CONSTRAINT `fk_category_id`
-  FOREIGN KEY (`category_id`)
-  REFERENCES `gamebar`.`categories` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
+ALTER TABLE products
+ADD FOREIGN KEY (category_id)
+REFERENCES categories(id);
