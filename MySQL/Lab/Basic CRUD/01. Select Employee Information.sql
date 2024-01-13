8be292490bc36f56dc4dc26bf964eec32089ed83
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-01-13 13:36:40 +0200

    Initial Commit

--- MySQL/Lab/Basic CRUD/01. Select Employee Information.sql
+++ MySQL/Lab/Basic CRUD/01. Select Employee Information.sql
@@ -1,1 +1,5 @@
-SELECT id, first_name, last_name, job_title FROM employees ORDER BY id;
+SELECT 
+    id, first_name, last_name, job_title
+FROM
+    employees
+ORDER BY id;
--- MySQL/Lab/Basic CRUD/04. Top Paid Employee.sql
+++ MySQL/Lab/Basic CRUD/04. Top Paid Employee.sql
@@ -1,5 +1,10 @@
 CREATE VIEW v_top_paid AS
-SELECT * FROM employees ORDER BY salary DESC LIMIT 1;
+    SELECT 
+        *
+    FROM
+        employees
+    ORDER BY salary DESC
+    LIMIT 1;
 SELECT 
     *
 FROM
