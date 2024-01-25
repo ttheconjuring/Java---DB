e09638401bd4d56c9aed4b0679efe96a40176543
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-01-25 20:44:28 +0200

    100/100

---
+++ MySQL/Exercise/Table Relations/03. Many-To-Many Relationship.sql
@@ -0,0 +1,25 @@
+CREATE TABLE students (
+    student_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE exams (
+    exam_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE students_exams (
+    student_id INT NOT NULL,
+    exam_id INT NOT NULL,
+    CONSTRAINT pk_students_exams PRIMARY KEY (student_id , exam_id),
+    CONSTRAINT fk_student_id FOREIGN KEY (student_id)
+        REFERENCES students (student_id),
+    CONSTRAINT fk_exam_id FOREIGN KEY (exam_id)
+        REFERENCES exams (exam_id)
+);
+
+INSERT INTO students (name) VALUES ('Mila'), ('Toni'), ('Ron');
+INSERT INTO exams (exam_id, name) VALUES (101, 'Spring MVC'), (102, 'Neo4j'), (103, 'Oracle 11g');
+INSERT INTO students_exams (student_id, exam_id) VALUES (1, 101), (1, 102), (2, 101), (3, 103), (2, 102), (2, 103);
+
+
