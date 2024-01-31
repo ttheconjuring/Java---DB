292d81b66636bd3257812e9e2efe9515aeb5bd0b
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-01-31 19:37:13 +0200

    100/100

---
+++ MySQL/Lab/Database Programmability and Transactions/1. Count Employees by Town.sql
@@ -0,0 +1,14 @@
+DELIMITER $
+
+CREATE FUNCTION ufn_count_employees_by_town(town_name VARCHAR(50)) 
+RETURNS INT
+DETERMINISTIC
+BEGIN
+	DECLARE count INT;
+	SET count := (SELECT COUNT(*) FROM employees e
+	JOIN addresses a ON e.address_id = a.address_id
+	JOIN towns t ON a.town_id = t.town_id
+	WHERE t.name = town_name);
+	RETURN count;
+END $
+
