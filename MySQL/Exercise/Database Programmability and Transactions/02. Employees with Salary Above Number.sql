41e965b7254545e2d1f17a6e2474fd09eeef56cd
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-02-02 10:14:56 +0200

    100/100

---
+++ MySQL/Exercise/Database Programmability and Transactions/02. Employees with Salary Above Number.sql
@@ -0,0 +1,11 @@
+CREATE PROCEDURE usp_get_employees_salary_above(number DECIMAL(10, 4))
+BEGIN
+	SELECT 
+		first_name, last_name
+	FROM
+		employees
+	WHERE
+		salary >= number
+	ORDER BY first_name , last_name , employee_id;
+END
+
